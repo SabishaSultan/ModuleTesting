c0d4bb96a31396ba12dcfdd2a7d6cf1d742d3f48
Sabina Sultanova <sultanovasabina89@gmail.com>
2025-06-30 07:43:00 +0300

    add WireMock

--- src/main/java/edu/innotech/Student.java
+++ src/main/java/edu/innotech/Student.java
@@ -1,12 +1,21 @@
 package edu.innotech;
 
-import java.util.Collections;
+import lombok.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
+import wiremock.org.apache.http.HttpEntity;
+import wiremock.org.apache.http.client.methods.CloseableHttpResponse;
+import wiremock.org.apache.http.client.methods.HttpGet;
+import wiremock.org.apache.http.impl.client.CloseableHttpClient;
+import wiremock.org.apache.http.impl.client.HttpClients;
+import wiremock.org.apache.http.util.EntityUtils;;
 
+@ToString
+@EqualsAndHashCode
 public class Student {
 
+    @Getter
+    @Setter
     private String name;
     private List<Integer> grades = new ArrayList<>();
 
@@ -14,53 +23,28 @@ public class Student {
         this.name = name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
     public List<Integer> getGrades() {
         return new ArrayList<>(grades);
     }
 
+    @SneakyThrows
     public void addGrade(int grade) {
-        if (grade < 2 || grade > 5) {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpGet request = new HttpGet("http://localhost:5352/checkGrade?grade=" + grade);
+        CloseableHttpResponse httpResponse = httpClient.execute(request);
+        HttpEntity entity = httpResponse.getEntity();
+        if(!Boolean.parseBoolean(EntityUtils.toString(entity))){
             throw new IllegalArgumentException(grade + " is wrong grade");
         }
         grades.add(grade);
     }
 
-    @Override
-    public int hashCode() {
-        int hash = 7;
-        hash = 13 * hash + Objects.hashCode(this.name);
-        hash = 13 * hash + Objects.hashCode(this.grades);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Student other = (Student) obj;
-        if (!Objects.equals(this.name, other.name)) {
-            return false;
-        }
-        return Objects.equals(this.grades, other.grades);
-    }
-
-    @Override
-    public String toString() {
-        return "Student{" + "name=" + name + ", marks=" + grades + '}';
+    @SneakyThrows
+    public int raiting() {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpGet request = new HttpGet("http://localhost:5352/educ?sum="+grades.stream().mapToInt(x->x).sum());
+        CloseableHttpResponse httpResponse = httpClient.execute(request);
+        HttpEntity entity = httpResponse.getEntity();
+        return Integer.parseInt(EntityUtils.toString(entity));
     }
 }
--- src/main/java/edu/innotech/Tests.java
+++ src/main/java/edu/innotech/Tests.java
@@ -1,105 +1,70 @@
 package edu.innotech;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
+import com.github.tomakehurst.wiremock.WireMockServer;
 import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
-import java.util.Arrays;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import java.util.List;
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static org.junit.jupiter.api.Assertions.*;
 
 
 public class Tests {
-    @Test
-    @Tag("smoke")
-    @DisplayName("Проверка записи оценок")
-    public void testAddGrade() {
+    private WireMockServer wireMockServer;
+    private Student stud;
 
-        Student stud = new Student("vasia");
-        List<Integer> grades = stud.getGrades();
-        stud.addGrade(3);
-        stud.addGrade(4);
-        stud.addGrade(5);
-        Assertions.assertEquals(2, grades.size());
-        Assertions.assertTrue(grades.contains(3));
-        Assertions.assertTrue(grades.contains(4));
-        Assertions.assertTrue(grades.contains(5));
+    @BeforeEach
+    public void SetUp() {
+        // Запускаем WireMock сервер
+        wireMockServer = new WireMockServer(5352);
+        wireMockServer.start();
 
-        System.out.println("Тест \"Проверка записи оценок\"");
+        // Создаем студента
+        stud = new Student("vasia");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        // Останавливаем WireMock сервер
+        wireMockServer.stop();
     }
 
     @Test
-    @Tag("critical")
-    @DisplayName("Проверка возвращения копии списка оценок")
-    public void testGetGradesReturnsCopy() {
-        Student stud = new Student("vasia");
-        stud.addGrade(3);
 
-        List<Integer> grades = stud.getGrades();
+    public void testAddValidGrade() throws Exception {
+            // Настраиваем заглушку для проверки оценки
+            wireMockServer.stubFor(get(urlEqualTo("/checkGrade?grade=5"))
+                    .willReturn(aResponse().withBody("true")));
 
-        // Проверяем, что можно получить оценки
-        Assertions.assertEquals(1, grades.size());
+            // Добавляем оценку
+            stud.addGrade(5);
 
-        // Изменяем копию и проверяем, что оригинальный список не изменился
-        grades.add(5);
+            // Проверяем, что оценка добавлена
+            List<Integer> grades = stud.getGrades();
+            assertEquals(1, grades.size());
+            assertEquals(5, grades.get(0));
+        }
 
-        Assertions.assertEquals(1, stud.getGrades().size());
+        @Test
+        public void testAddInvalidGrade () {
+            // Настраиваем заглушку для проверки оценки
+            wireMockServer.stubFor(get(urlEqualTo("/checkGrade?grade=11"))
+                    .willReturn(aResponse().withBody("false")));
+
+            // Проверяем, что при добавлении неправильной оценки выбрасывается исключение
+            Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+                stud.addGrade(11);
+            });
+
+            String expectedMessage = "11 is wrong grade";
+            String actualMessage = exception.getMessage();
+
+            assertTrue(actualMessage.contains(expectedMessage));
+
+            // Проверяем, что оценка не была добавлена
+            List<Integer> grades = stud.getGrades();
+            assertTrue(grades.isEmpty());
+        }
     }
-
-    @Test
-    @Tag("smoke")
-    @DisplayName("Проверка невалидных оценок")
-    public void marksNotInRange() {
-        List<Integer> lst = Arrays.asList(0, 1, 6, 7);
-        Student stud = new Student("vasia");
-        Assertions.assertThrows(IllegalArgumentException.class, () -> stud.addGrade(lst.get(0)));
-        Assertions.assertThrows(IllegalArgumentException.class, () -> stud.addGrade(lst.get(1)));
-        Assertions.assertThrows(IllegalArgumentException.class, () -> stud.addGrade(lst.get(2)));
-        Assertions.assertThrows(IllegalArgumentException.class, () -> stud.addGrade(lst.get(3)));
-    }
-    @Test
-    @Tag("critical")
-    @DisplayName("Обработка исключений при добавлении неправильных оценок.")
-    public void testAddInvalidGrade() {
-        Student stud = new Student("vasia");
-
-        Exception exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            stud.addGrade(6); // Неправильная оценка
-        });
-
-        Assertions.assertEquals("6 is wrong grade", exception.getMessage());
-    }
-
-    @Test
-    @Tag("critical")
-    @DisplayName("Правильность методов equals и hashCode.")
-    public void testEqualsAndHashCode() {
-        Student stud1 = new Student("vasia");
-        Student stud2 = new Student("sasha");
-
-        stud1.addGrade(3);
-        stud2.addGrade(4);
-
-        Assertions.assertEquals(stud1, stud2);
-        Assertions.assertEquals(stud1.hashCode(), stud2.hashCode());
-
-        stud2.addGrade(4);
-
-        Assertions.assertNotEquals(stud1, stud2);
-    }
-
-    @Test
-    @Tag("critical")
-    @DisplayName("Корректное представление объекта в строковом формате")
-    public void testToString() {
-        Student stud = new Student("vasia");
-        stud.addGrade(3);
-
-        String expectedString = "Student{name='vasia', marks=[3]}";
-
-        Assertions.assertEquals(expectedString, stud.toString());
-    }
-}
-
